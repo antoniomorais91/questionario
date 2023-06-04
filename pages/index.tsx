6810495ead17920ac6e83c62b1097bb4a4538cc0
antoniomorais91 <antonio.vinicius.morais@gmail.com>
2023-06-03 23:34:38 -0300

    terceirocommit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 import Questionario from '@/components/Questionario'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://questionario-delta.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
